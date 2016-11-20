48bbd07164579ff5cd566b71490f00db50c90bd3
Flawedspirit <cyberfrag11@hotmail.com>
2016-11-20 11:27:36 -0500

    Licensing crap. It's GPLv3, so go nuts

--- src/main/java/com/flawedspirit/sandboxmod/SandboxMod.java
+++ src/main/java/com/flawedspirit/sandboxmod/SandboxMod.java
@@ -1,3 +1,10 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Flawedspirit.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v3
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.txt
+ ******************************************************************************/
 package com.flawedspirit.sandboxmod;
 
 import com.flawedspirit.sandboxmod.proxy.CommonProxy;
