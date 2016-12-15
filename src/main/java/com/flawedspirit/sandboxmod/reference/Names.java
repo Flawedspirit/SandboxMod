638a1a6f7ab8e6ac8db9dec5f082fcd13b660bd5
Flawedspirit <cyberfrag11@hotmail.com>
2016-12-15 03:37:08 -0500

    Added centralized constants for common IDs and names

---
+++ src/main/java/com/flawedspirit/sandboxmod/reference/Names.java
@@ -0,0 +1,28 @@
+package com.flawedspirit.sandboxmod.reference;
+
+public class Names {
+	
+	public static class Blocks {
+		public static final String BLOCK_EXPERIMENTIUM = "blockExperimentium";
+		public static final String ORE_EXPERIMENTIUM = "oreExperimentium";
+		public static final String COUNTER = "counter";
+	}
+	
+	public static class Config {
+		public static final String EXPERIMENTIUM_HELMET_ENABLED = "experimentium_helmet_enabled";
+		public static final String EXPERIMENTIUM_CHEST_ENABLED = "experimentium_chestplate_enabled";
+		public static final String EXPERIMENTIUM_LEGS_ENABLED = "experimentium_leggings_enabled";
+		public static final String EXPERIMENTIUM_BOOTS_ENABLED = "experimentium_boots_enabled";
+		public static final String GENERATE_EXPERIMENTIUM_ORE = "generate_experimentium_ore";
+		public static final String FORCE_RETRO_GEN = "force_retro_gen";
+	}
+	
+	public static class Items {
+		public static final String EXPERIMENTIUM_HELMET = "experimentiumHelmet";
+		public static final String EXPERIMENTIUM_CHEST = "experimentiumChestplate";
+		public static final String EXPERIMENTIUM_LEGS = "experimentiumLeggings";
+		public static final String EXPERIMENTIUM_BOOTS = "experimentiumBoots";
+		public static final String EXPERIMENTIUM_INGOT = "ingotExperimentium";
+		public static final String EXPERIMENTIUM_NUGGET = "nuggetExperimentium";
+	}
+}
