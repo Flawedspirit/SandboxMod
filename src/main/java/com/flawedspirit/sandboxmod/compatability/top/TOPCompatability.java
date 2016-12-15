337f350f182f2101504ae1c7b0399d57f2347bfb
Flawedspirit <cyberfrag11@hotmail.com>
2016-12-14 23:57:26 -0500

    Added The One Probe and WAILA compatability.

---
+++ src/main/java/com/flawedspirit/sandboxmod/compatability/top/TOPCompatability.java
@@ -0,0 +1,51 @@
+package com.flawedspirit.sandboxmod.compatability.top;
+
+import com.flawedspirit.sandboxmod.reference.Reference;
+import com.google.common.base.Function;
+
+import mcjty.theoneprobe.api.IProbeHitData;
+import mcjty.theoneprobe.api.IProbeInfo;
+import mcjty.theoneprobe.api.IProbeInfoProvider;
+import mcjty.theoneprobe.api.ITheOneProbe;
+import mcjty.theoneprobe.api.ProbeMode;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+public class TOPCompatability {
+	
+	private static boolean isRegistered;
+	
+	public static void register() {
+		if(!isRegistered) {
+			FMLInterModComms.sendFunctionMessage("theoneprobe", "getTheOneProbe", "com.flawedspirit.sandboxmod.compatability.top.TOPCompatability$InitTheOneProbe");
+			isRegistered = true;
+		}
+	}
+	
+	public static class InitTheOneProbe implements Function<ITheOneProbe, Void> {
+
+		@Override
+		public Void apply(ITheOneProbe theOneProbe) {
+			theOneProbe.registerProvider(new IProbeInfoProvider() {
+
+				@Override
+				public String getID() {
+					return Reference.MODID + ":default";
+				}
+
+				@Override
+				public void addProbeInfo(ProbeMode mode, IProbeInfo probeInfo, EntityPlayer player, World world,
+						IBlockState blockState, IProbeHitData data) {
+					if(blockState.getBlock() instanceof ITOPInfoProvider) {
+						ITOPInfoProvider provider = (ITOPInfoProvider) blockState.getBlock();
+						provider.addProbeInfo(mode, probeInfo, player, world, blockState, data);
+					}
+				}
+			});
+			return null;
+		}
+		
+	}
+}
---
+++ src/main/java/com/flawedspirit/sandboxmod/compatability/waila/WailaCompatability.java
@@ -0,0 +1,83 @@
+package com.flawedspirit.sandboxmod.compatability.waila;
+
+import java.util.List;
+
+import jline.internal.Log;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import mcp.mobius.waila.api.IWailaRegistrar;
+import net.minecraft.block.Block;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.event.FMLInterModComms;
+
+public class WailaCompatability implements IWailaDataProvider {
+	
+	//private static final WailaCompatability INSTANCE = new WailaCompatability();
+	private static boolean isRegistered;
+	private static boolean isLoaded;
+	
+	private WailaCompatability() {}
+	
+	public static void load(IWailaRegistrar registrar) {
+		Log.debug("WailaCompatability.load");
+		
+		 if(!isRegistered) {
+			 throw new RuntimeException("WAILA compatability handler not properly registered! I suspect this is a bad thing!");
+		 }
+		 
+		 if(!isLoaded) {
+			 //registrar.registerHeadProvider(INSTANCE, DataBlock.class);
+			 isLoaded = true;
+		 }
+	}
+	
+	public static void register() {
+		if(!isRegistered) {
+			FMLInterModComms.sendMessage("Waila", "register", "com.flawedspirit.sandboxmod.compatability.top.WailaCompatability.load");
+			isRegistered = true;
+		}
+	}
+
+	@Override
+	public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+		return null;
+	}
+	
+
+
+	@Override
+	public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		return currenttip;
+	}
+
+	@Override
+	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		Block block = accessor.getBlock();
+		
+		if(block instanceof IWailaInfoProvider) {
+			return ((IWailaInfoProvider) block).getWailaBody(itemStack, currenttip, accessor, config);
+		}
+		return currenttip;
+	}
+
+	@Override
+	public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor,
+			IWailaConfigHandler config) {
+		return currenttip;
+	}
+
+	@Override
+	public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world,
+			BlockPos pos) {
+		return tag;
+	}
+
+}
